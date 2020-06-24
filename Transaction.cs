ba4029d12fba57e510bd51afdb680e021483400d
vamsishankar21 <67362450+vamsishankar21@users.noreply.github.com>
2020-06-24 12:57:23 +0530

    Add files via upload

---
+++ Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Transaction
+    {
+        public int Amount { get; set; }
+        public string Note { get; set; }
+        public Transaction(int amount, string note)
+        {
+            this.Amount = amount;
+            
+            this.Note = note;
+        }
+    }
+}
