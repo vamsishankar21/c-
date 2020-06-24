7a3d8d1f655413011b8691f647ba1ee425c6ff5b
vamsishankar21 <67362450+vamsishankar21@users.noreply.github.com>
2020-06-24 12:57:00 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ");
+            var account = new BankAccount("Vamsi", 1000);
+            System.Console.WriteLine("Account created");
+            account.Deposit(3000, "Added from friende");
+            account.Withdraw(100, "Withdrawed");
+            Console.WriteLine(account.Balance);
+            Console.WriteLine("Withdrawed");
+            account.Withdraw(120, "bought online");
+            Console.WriteLine(account.Balance);
+            Console.ReadLine();
+
+        }
+    }
+}
