a047ca11e0c6272f6eee3678dafa2befa64d6b15
vamsishankar21 <67362450+vamsishankar21@users.noreply.github.com>
2020-06-24 12:56:32 +0530

    Add files via upload

---
+++ BankAccount.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class BankAccount
+    {
+        private List<Transaction> transactions = new List<Transaction>();
+        public long AccountNumber { get; }
+        public string Owner { get; set; }
+        public bool Status { get; set; } = true;
+        public int Balance
+        {
+            get
+            {
+                int balance = 0;
+                foreach (var trans in transactions)
+                {
+                    balance = balance + trans.Amount;
+                }
+                return balance;
+            }
+        }
+        public void Deposit(int amount, string note)
+        {
+            var deposit = new Transaction(amount, note);
+            transactions.Add(deposit);
+        }
+        public void Withdraw(int amount, string note)
+        {
+            var withDraw = new Transaction(-amount, note);
+                transactions.Add(withDraw);
+        }
+        private long GenerateAccountNumber()
+        {
+            Random rand = new Random();
+            return rand.Next(1000);
+        }
+        public BankAccount(string owner, int initialBalance)
+        {
+            this.Owner = owner;
+            this.Deposit(initialBalance, "Initial Balance");
+            this.AccountNumber = GenerateAccountNumber();
+            System.Console.WriteLine($"Object initialized with : \n AccountNumber: {this.AccountNumber} \n Owner: {this.Owner} \n Balance: {this.Balance} \n Status: {this.Status}");
+
+        }
+
+    }
+}
